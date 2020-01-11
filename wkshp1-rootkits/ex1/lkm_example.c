c4aa0396a8a9f72bbb33b81018662533f62d94c8
uttie <uttieg@gmail.com>
2020-01-11 12:03:35 -0800

    adding first exercise

---
+++ wkshp1-rootkits/ex1/lkm_example.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/* Blantant copied from: https://blog.sourcerer.io/writing-a-simple-linux-kernel-module-d9dc3762c234
+ */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Robert W. Oliver II");
+MODULE_DESCRIPTION("A simple example Linux module.");
+MODULE_VERSION("0.01");
+
+static int __init lkm_example_init(void) {
+ printk(KERN_INFO "Hello, World!\n");
+ return 0;
+}
+
+static void __exit lkm_example_exit(void) {
+ printk(KERN_INFO "Goodbye, World!\n");
+}
+
+module_init(lkm_example_init);
+module_exit(lkm_example_exit);
